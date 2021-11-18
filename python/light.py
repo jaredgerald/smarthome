cf68abc64a84b255846482b1bda744b5ef9c4012
Sedrik Staack <sedrik.staack@sidion.de>
2021-11-18 18:04:52 +0100

    python file changes

--- python/light.py
+++ python/light.py
@@ -4,40 +4,41 @@ import argparse
 import configparser
 import time
 
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-
-    for t in args.topics:
-        client.subscribe(t)
+    for topic in args.topics:
+        client.subscribe(topic)
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.payload))
 
     # code to call lightstripe here
 
-#----------------------------------------------------------------------------------------------------------
 
-def colorWipe(strip, color, wait_ms=50):
+# ----------------------------------------------------------------------------------------------------------
+
+def color_wipe(strip, color, wait_ms=50):
     """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
         strip.show()
-        time.sleep(wait_ms/1000.0)
+        time.sleep(wait_ms / 1000.0)
 
-def rainbowCycle(strip, wait_ms=20, iterations=5):
+
+def rainbow_cycle(strip, wait_ms=20, iterations=5):
     """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256*iterations):
+    for j in range(256 * iterations):
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
         strip.show()
-        time.sleep(wait_ms/1000.0)
-                
+        time.sleep(wait_ms / 1000.0)
+
+
 def wheel(pos):
     """Generate rainbow colors across 0-255 positions."""
     if pos < 85:
@@ -49,23 +50,25 @@ def wheel(pos):
         pos -= 170
         return Color(0, pos * 3, 255 - pos * 3)
 
-#----------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------
 
 # Process arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--username', action='store', dest='username', help='The mqtt username.')
-parser.add_argument('-p', '--password', action='store', dest='password', help='The mqtt password.')
-parser.add_argument('-t','--topics', nargs='+', help='<Required> Subscribe to topics', required=True)
+parser.add_argument('-P', '--password', action='store', dest='password', help='The mqtt password.')
+parser.add_argument('-t', '--topics', nargs='+', help='<Required> Subscribe to topics', required=True)
+#parser.add_argument('-c', '--config', action='store', default='', dest='config', help='The light strip config')
 args = parser.parse_args()
 
-#----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------
 
 # Read config
 config = configparser.ConfigParser()
 config.read(r'/home/pi/smarthome/light/config.cfg')
-    
+
 led_strip_configuration_name = 'led_strip_configuration'
-    
+
 LED_COUNT = int(config.get(led_strip_configuration_name, 'led_count'))
 LED_PIN = int(config.get(led_strip_configuration_name, 'led_pin'))
 LED_FREQ_HZ = int(config.get(led_strip_configuration_name, 'led_freq_hz'))
@@ -74,20 +77,20 @@ LED_INVERT = config.get(led_strip_configuration_name, 'led_invert') == 'True'
 LED_BRIGHTNESS = int(config.get(led_strip_configuration_name, 'led_brightness'))
 LED_CHANNEL = int(config.get(led_strip_configuration_name, 'led_channel'))
 
-#----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------
 
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
-#----------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.username_pw_set(username = args.username, password = args.password)
+client.username_pw_set(username=args.username, password=args.password)
 
 client.connect("127.0.0.1", 1883, 60)
 
@@ -97,7 +100,8 @@ client.connect("127.0.0.1", 1883, 60)
 # manual interface.
 client.loop_forever()
 
-#----------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------
 
 def rainbow():
     try:
@@ -106,4 +110,4 @@ def rainbow():
 
     except KeyboardInterrupt:
         if args.clear:
-            colorWipe(strip, Color(0,0,0), 10)
+            colorWipe(strip, Color(0, 0, 0), 10)
--- python/temp_sensor.py
+++ python/temp_sensor.py
@@ -4,17 +4,21 @@ import Adafruit_DHT
 import time
 from datetime import datetime
 
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
+
 # Process arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--username', action='store', dest='username', help='The mqtt username.')
-parser.add_argument('-p', '--password', action='store', dest='password', help='The mqtt password.')
+parser.add_argument('-P', '--password', action='store', dest='password', help='The mqtt password.')
 parser.add_argument('-t', '--topic', action='store', dest='topic', help='The mqtt topic to publish to.')
-parser.add_argument('-s', '--seconds', type=float, action='store', dest='seconds', help='The intervall to publish')
-parser.add_argument('-n', '--pin', type=int, action='store', dest='pin', help='The sensor pin')
+parser.add_argument('-i', '--ip', action='store', dest='ip', default='127.0.0.1', help='The mqtt Server ip')
+parser.add_argument('-p', '--port', type=int, action='store', dest='port', default=1883, help='The mqtt Server port')
+parser.add_argument('-s', '--seconds', type=float, action='store', dest='seconds', help='The interval to publish')
+parser.add_argument('-n', '--pin', type=int, action='store', default=4, dest='pin', help='The sensor pin')
 args = parser.parse_args()
 
 DHT_SENSOR = Adafruit_DHT.AM2302
@@ -22,11 +26,11 @@ DHT_SENSOR = Adafruit_DHT.AM2302
 client = mqtt.Client()
 client.on_connect = on_connect
 
-client.username_pw_set(username = args.username, password = args.password)
+client.username_pw_set(username=args.username, password=args.password)
 
-client.connect("127.0.0.1", 1883, 60)
+client.connect(args.ip, args.port, 60)
 
-client.loop_start() #start the loop
+client.loop_start()  # start the loop
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, args.pin)
@@ -40,6 +44,4 @@ while True:
     else:
         print("Failed to retrieve data from humidity sensor")
 
-    time.sleep(args.seconds) # wait
-
-#client.loop_stop() #stop the loop
+    time.sleep(args.seconds)  # wait
