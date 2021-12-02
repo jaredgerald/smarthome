7bae17fa649c9dfeb56c8b4e7bb9c13e5ce845f4
Sedrik Staack <sedrik.staack@sidion.de>
2021-12-02 13:58:47 +0100

    esp programmable light config

---
+++ ESP-8266/Light/ProgrammableLightProgram/arduino_local_config.h
@@ -0,0 +1,10 @@
+#define h_wifi_ssid "<DEIN WIFI SSID>"
+#define h_wifi_password "<DEIN WIFI PASSWORT>"
+#define h_mqttbroker_host "<DEINE IP VON MQTT BROKER>"
+#define h_mqtt_user "<DEIN MQTT Name>"
+#define h_mqtt_password "<DEIN MQTT Password>"
+#define h_device_id "<Die ID, welche du von Springboot bekommen hast>"
+#define h_publish_interval_s <Interval bis das Geraet eine Statusmeldung schickt>
+#define h_outputPin <Der Output Pin, an dem die LED-Leiste befestigt wurde>
+#define h_num_led <Die Nummer von leds, die dein led-streifen hat>
+
