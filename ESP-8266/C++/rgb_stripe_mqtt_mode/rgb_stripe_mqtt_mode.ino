11a23e74a4332f1839077de2c02093b5dbdd3712
Sedrik Staack <sedrik.staack@sidion.de>
2021-12-03 09:23:38 +0100

    rgb_stripe_mqtt_mode.ino v1

--- ESP-8266/C++/rgb_stripe_mqtt_mode/rgb_stripe_mqtt_mode.ino
+++ ESP-8266/C++/rgb_stripe_mqtt_mode/rgb_stripe_mqtt_mode.ino
@@ -5,7 +5,7 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <sstream>
-#include "../local_config.h"
+#include "local_config.h"
 
 #define NTP_OFFSET   60 * 60      // In seconds
 #define NTP_INTERVAL 60 * 1000    // In miliseconds
@@ -35,12 +35,17 @@ const int blueOutputPin = h_blue_pin;
 // Mode topic like: "{\"mode\":\"static\",\"color\":{\"r\": 25, \"g\":25, \"b\":25}}"
 const String modeTopic = "light/" +  deviceID + "/mode";
 
-unsigned long previousMillisPublish = 0; 
+unsigned long previousMillisPublish = 0;
 const long intervalPublish = h_publish_interval_s * 1000;
 
 unsigned long previousMillisMode = 0; 
 unsigned long intervalMode = 1;
-bool modeDone = false;
+
+int colorRed = 0;
+int colorGreen = 0;
+int colorBlue = 0;
+
+int direction = 1;
 
 DynamicJsonDocument currentMode(1024);
 
@@ -59,6 +64,14 @@ void setup() {
   // setup environments
   setup_wifi();
   setup_mqtt();
+  setup_current_mode_json();
+}
+
+void setup_current_mode_json() {
+  currentMode["mode"] = "static";
+  currentMode["color"]["r"] = 200;
+  currentMode["color"]["g"] = 5;
+  currentMode["color"]["b"] = 100;
 }
 
 void setup_wifi() {
@@ -125,102 +138,198 @@ void mqtt_publish_time() {
 
 // On arriving message:
 void callback(char* topic, byte* message, unsigned int length) {
-  Serial.println("---------------------------------------");
-  Serial.print("Message arrived on topic: ");
+  Serial.println("-----------------");
+  Serial.print("Arrived topic: ");
   Serial.print(topic);
 
   // If topic is modeTopic
   if (String(topic) == modeTopic)
   {
-    String mode;
+    String json;
     for (int i = 0; i < length; i++) {
-      mode += (char)message[i];
+      json += (char)message[i];
     }
 
-    Serial.println("Arrived mode: ");
-    Serial.print(mode);
     Serial.println();
+    Serial.print("Arrived Json: ");
+    Serial.println(json);
 
-    deserializeJson(currentMode, mode);
+    deserializeJson(currentMode, json);
+
+    colorRed = 0;
+    colorGreen = 0;
+    colorBlue = 0;
+
+    execute_led_mode();
   }
   else {
     Serial.println("Error: Topic is not mode");
-  }  
-  Serial.println("---------------------------------------");
+  }
 }
 
-void mode_static(DynamicJsonDocument doc) {
-  int red = doc["color"]["r"];
-  int green = doc["color"]["g"];
-  int blue = doc["color"]["b"];
-
-  Serial.println(red);
-  Serial.println(green);
-  Serial.println(blue);
-
-  analogWrite(redOutputPin, red);
-  analogWrite(greenOutputPin, green);
-  analogWrite(blueOutputPin, blue);
-
-  modeDone = true;
+void mode_static() {
+  colorRed = currentMode["color"]["r"];
+  colorGreen = currentMode["color"]["g"];
+  colorBlue = currentMode["color"]["b"];
 }
 
-void mode_shuffle(DynamicJsonDocument doc) {
-  int speed = doc["speed"];
-  Serial.println(speed);
+void mode_shuffle() { //not good
+  if(colorRed <= 0) {
+    colorRed = 255;
+  }
+  else if (colorRed >= 255)
+  {
+    colorRed = 0;
+  }
+
+  if(colorGreen <= 0) {
+    colorGreen = 255;
+  }
+  else if (colorGreen >= 255)
+  {
+    colorGreen = 0;
+  }
+
+  if(colorBlue <= 0) {
+    colorBlue = 255;
+  }
+  else if (colorBlue >= 255)
+  {
+    colorBlue = 0;
+  }
+
+  double randRed = random(100);
+  double randGreen = random(100);
+  double randBlue = random(100);
+
+  if(randRed >= 50) {
+    colorRed = colorRed+1;
+  }
+  else {
+    colorRed = colorRed-1;
+  }
+
+  if(randGreen >= 50) {
+    colorGreen = colorGreen+1;
+  }
+  else {
+    colorGreen = colorGreen-1;
+  }
+
+  if(randBlue >= 50) {
+    colorBlue = colorBlue+1;
+  }
+  else {
+    colorBlue = colorBlue-1;
+  }
 }
 
-void mode_blink(DynamicJsonDocument doc) {
-  int speed = doc["speed"];
-  int red = doc["color"]["r"];
-  int green = doc["color"]["g"];
-  int blue = doc["color"]["b"];
-  Serial.println(red);
-  Serial.println(green);
-  Serial.println(blue);
-  Serial.println(speed);
+void mode_blink() {
+  if(colorRed == 0) {
+    colorRed = currentMode["color"]["r"];
+    colorGreen = currentMode["color"]["g"];
+    colorBlue = currentMode["color"]["b"];
+  }
+  else {
+    colorRed = 0;
+    colorGreen = 0;
+    colorBlue = 0;
+  }
 }
 
-void mode_fade(DynamicJsonDocument doc) {
-  int speed = doc["speed"];
-  Serial.println(speed);
+void mode_fade() { //not working
+  if(colorRed == 0) {
+    colorRed = currentMode["color"]["r"];
+    colorGreen = currentMode["color"]["g"];
+    colorBlue = currentMode["color"]["b"];
+  }
+
+  if(colorRed <= currentMode["color"]["r"]) {
+    colorRed + direction;
+  }
 }
 
-void mode_multicolor(DynamicJsonDocument doc) {
-  
+void mode_multicolor() {
+  bool isRed0 = colorRed == 0;
+  bool isGreen0 = colorGreen == 0;
+  bool isBlue0 = colorBlue == 0;
+  //Check if its start. 
+  if(isRed0 && isGreen0 && isBlue0) 
+  {
+    colorRed = currentMode["color"][0]["r"];
+    colorGreen = currentMode["color"][0]["g"];
+    colorBlue = currentMode["color"][0]["b"];
+  }
+  else
+  {
+    //Check if color is in color array. When in array, pick array +1.
+    for (int i = 0; i < sizeof(currentMode["color"]); i++)
+    {
+      bool isSameRed = colorRed == currentMode["color"][i]["r"];
+      bool isSameGreen = colorGreen == currentMode["color"][i]["g"];
+      bool isSameBlue = colorBlue == currentMode["color"][i]["b"];
+
+      Serial.println(sizeof(currentMode["color"]) / sizeof(currentMode["color"][0]));
+
+      if(isSameRed && isSameGreen && isSameBlue) {
+        if(i+1 == sizeof(currentMode["color"]) / sizeof(currentMode["color"][0])) { //line not working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          colorRed = currentMode["color"][0]["r"];
+          colorGreen = currentMode["color"][0]["g"];
+          colorBlue = currentMode["color"][0]["b"];
+        }
+        else {
+          colorRed = currentMode["color"][i+1]["r"];
+          colorGreen = currentMode["color"][i+1]["g"];
+          colorBlue = currentMode["color"][i+1]["b"];
+        }
+        break;
+      }
+    }
+  }
 }
 
 void execute_led_mode() {
   const String mode = String(currentMode["mode"]); 
 
-  unsigned long currentMillis = millis();
-
-  bool isMillisGreaterInterval = currentMillis - previousMillisMode >= intervalMode;
-
-  if(mode == "static" && intervalMode != 0) {
-    mode_static(currentMode);
-    intervalMode = 0;
+  if(mode == "static") {
+    mode_static();
+    intervalMode = 6000000;
   }
-  else if (mode == "shuffle" && isMillisGreaterInterval) {
-    mode_shuffle(currentMode);
-    intervalMode = doc["interval"];
+  else if (mode == "shuffle") {
+    mode_shuffle();
+    intervalMode = currentMode["interval"];
   }
-  else if (mode == "blink" && isMillisGreaterInterval) {
-    mode_blink(currentMode);
-    intervalMode = doc["interval"];
+  else if (mode == "blink") {
+    mode_blink();
+    intervalMode = currentMode["interval"];
   }
-  else if (mode == "fade" && isMillisGreaterInterval) {
-    mode_fade(currentMode);
-    intervalMode = doc["interval"];
+  else if (mode == "fade" ) {
+    mode_fade();
+    intervalMode = currentMode["interval"];
   }
-  else if (mode == "multicolor" && isMillisGreaterInterval) {
-    mode_multicolor(currentMode);
-    intervalMode = doc["interval"];
+  else if (mode == "multicolor") {
+    mode_multicolor();
+    intervalMode = currentMode["interval"];
   }
   else {
     Serial.println("Error: Mode not existing:");
     Serial.println(mode);
   }
+
+  //Output
+  analogWrite(redOutputPin, colorRed);
+  analogWrite(greenOutputPin, colorGreen);
+  analogWrite(blueOutputPin, colorBlue);
+
+  Serial.println("-----------------");
+  Serial.print("m=");
+  Serial.print(mode);
+  Serial.print(", r=");
+  Serial.print(colorRed);
+  Serial.print(", g=");
+  Serial.print(colorGreen);
+  Serial.print(", b=");
+  Serial.println(colorBlue);
 }
 
 void loop() {
@@ -231,6 +340,7 @@ void loop() {
 
   unsigned long currentMillis = millis(); 
 
+  //publisher timer
   if (currentMillis - previousMillisPublish >= intervalPublish)
   {
     previousMillisPublish = currentMillis;
@@ -239,5 +349,9 @@ void loop() {
     mqtt_publish_time();
   }
 
-  execute_led_mode();
+  //Led mode timer
+  if(currentMillis - previousMillisMode >= intervalMode) {
+    previousMillisMode = currentMillis;
+    execute_led_mode();
+  }
 }
